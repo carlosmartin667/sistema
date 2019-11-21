769e4af73c37294c43f8afacbc7ae1a3d6382d79
carlosmartin667 <carloscornejomoscoso@gmail.com>
2019-11-21 08:41:33 -0300

    esstilos

--- MaxiKiosco/MaxiKiosco/Controllers/HomeController.cs
+++ MaxiKiosco/MaxiKiosco/Controllers/HomeController.cs
@@ -23,10 +23,17 @@ namespace MaxiKiosco.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Galeria()
         {
             return View();
         }
+
+
+        public IActionResult Contactenos()
+        {
+            return View();
+        }
+  
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
